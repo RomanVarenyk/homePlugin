9616453c2c001b935bd9300e5348971b7cc7dc1f
Ukraine1449 <roma.bystriakov@gmail.com>
2022-01-26 20:29:11 -0500

    Working to

--- src/main/java/com/github/ukraine1449/home/Home.java
+++ src/main/java/com/github/ukraine1449/home/Home.java
@@ -1,5 +1,8 @@
 package com.github.ukraine1449.home;
 
+import com.github.ukraine1449.home.commands.goHome;
+import com.github.ukraine1449.home.commands.playerJoinEvent;
+import com.github.ukraine1449.home.commands.setHome;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,8 +10,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public final class Home extends JavaPlugin {
-public boolean preferStored = true;
+public boolean preferStored = true; // if set to true in plugin.yml this will make the block bed preffered if there is block bed and /sethome set.
     @Override
     public void onEnable() {
         updatePreferredStore();
@@ -20,8 +26,9 @@ public boolean preferStored = true;
         } catch (Exception e) {
             e.printStackTrace();
         }//Creates table in the SQL DB if not already existing
-//        getCommand("duel").setExecutor(new Duel(this));
-//        getServer().getPluginManager().registerEvents(new playerKillEvent(this), this);
+        getCommand("home").setExecutor(new goHome(this));
+        getCommand("setHome").setExecutor(new setHome(this));
+       getServer().getPluginManager().registerEvents(new playerJoinEvent(this), this);
     }
 
     @Override
@@ -57,7 +64,7 @@ public boolean preferStored = true;
     public void createTableUserStats()throws Exception{
         try{
             Connection con = getConnection();
-            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS userHomes(UUID varchar(255),bedLoc varchar(255),hasBed int, PRIMARY KEY (UUID))");
+            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS userHomes(UUID varchar(255),bedLoc varchar(255),worldName varchar(255),hasBed varchar(255), PRIMARY KEY (UUID))");
             create.executeUpdate();
             con.close();// create table named userStats, with UUID as a string and also primary ID, along with wonGames, lostGames, kills and deaths as ints
         }catch(Exception e){}
@@ -73,7 +80,14 @@ public boolean preferStored = true;
             e.printStackTrace();
         }try{
             Connection con = getConnection();
-            PreparedStatement posted = con.prepareStatement("UPDATE userHomes SET hasBed=1 WHERE UUID='"+UUID+"'");
+            PreparedStatement posted = con.prepareStatement("UPDATE userHomes SET hasBed='true' WHERE UUID='"+UUID+"'");
+            posted.executeUpdate();
+            con.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }try{
+            Connection con = getConnection();
+            PreparedStatement posted = con.prepareStatement("UPDATE userHomes SET world='"+bedLoc.getWorld().getName()+"' WHERE UUID='"+UUID+"'");
             posted.executeUpdate();
             con.close();
         }catch (Exception e){
@@ -84,11 +98,47 @@ public boolean preferStored = true;
         String UUID = player.getUniqueId().toString();
         try{//executed when a player is joining in playerJoinEvent, basically if the players UUID isnt already in the database it adds it with all stats of 0
             Connection con = getConnection();
-            PreparedStatement posted = con.prepareStatement("INSERT INTO userHomes(UUID, bedLoc, hasBed) VALUES ('"+UUID+"', 0, 0)ON DUPLICATE KEY UPDATE UUID='"+UUID+"'");
+            PreparedStatement posted = con.prepareStatement("INSERT INTO userHomes(UUID, bedLoc, worldName, hasBed) VALUES ('"+UUID+"', false, false, false)ON DUPLICATE KEY UPDATE UUID='"+UUID+"'");
             posted.executeUpdate();
             con.close();
         }catch (Exception e){
             e.printStackTrace();
         }
+    }   public ArrayList<String> selectCD(String UUID) throws Exception {
+        ArrayList<String> retuns = new ArrayList<String>();
+        Connection con = getConnection();
+        PreparedStatement statement = con.prepareStatement("SELECT bedLoc,hasBed,worldName FROM userHomes WHERE UUID='"+UUID+"'");
+        ResultSet result = statement.executeQuery();
+        while(result.next()){
+            retuns.add(result.getString("hasBed"));
+            retuns.add(result.getString("bedLoc"));
+            retuns.add(result.getString("worldName"));
+        }
+        con.close();
+        return retuns;
+    }
+    public Location getBedLoc(Player player){
+        Location homeLoc = player.getLocation();
+        ArrayList<String> rawSQL = new ArrayList<String>();
+        try {
+            rawSQL = selectCD(player.getUniqueId().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            homeLoc.setWorld(getServer().getWorld(rawSQL.get(2)));
+            String[] split = rawSQL.get(1).split(",");
+            homeLoc.setX(Double.parseDouble(split[1]));
+            homeLoc.setY(Double.parseDouble(split[2]));
+            homeLoc.setZ(Double.parseDouble(split[3]));
+            System.out.println(Double.parseDouble(split[3]));
+            System.out.println(Double.parseDouble(split[2]));
+            System.out.println(Double.parseDouble(split[1]));
+            System.out.println(homeLoc);
+            System.out.println(rawSQL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return homeLoc;
     }
 }
--- src/main/java/com/github/ukraine1449/home/commands/goHome.java
+++ src/main/java/com/github/ukraine1449/home/commands/goHome.java
@@ -3,11 +3,13 @@ package com.github.ukraine1449.home.commands;
 import com.github.ukraine1449.home.Home;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class goHome implements CommandExecutor {
@@ -21,12 +23,19 @@ Home plugin;
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player){
             Player player = (Player) sender;
+            String hasBed = null;
+            try {
+                hasBed = plugin.selectCD(player.getUniqueId().toString()).get(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             if(plugin.preferStored){
-
                 if(!(player.getBedSpawnLocation() == null)){
                     Location playerBedLoc = player.getBedSpawnLocation();
                     player.teleport(playerBedLoc);
                     player.sendMessage(ChatColor.GREEN + "You have been teleported to your bed location");
+                }else if (!hasBed.equals("false")){
+                    player.teleport(plugin.getBedLoc(player));
                 }else{
                     player.sendMessage(ChatColor.RED + "You do not have a bed or a home set.");
                 }
